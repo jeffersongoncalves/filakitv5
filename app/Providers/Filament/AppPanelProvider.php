d5f12c339f1bb65a3831e8954f87f1366ff314c7
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2026-01-23 23:13:06 -0300

    **build(dependencies): add filament-edit-profile and update dependencies**
    Added `joaopaulolndev/filament-edit-profile` package to dependencies. Updated and regenerated `composer.lock` with new packages including `jaybizzle/crawler-detect`, `jenssegers/agent`, and `mobiledetect/mobiledetectlib`. Enabled and configured the `FilamentEditProfilePlugin` in `AppPanelProvider` and `AdminPanelProvider`.

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\App\Pages\Auth\Login;
+use Filament\Actions\Action;
 use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -20,6 +21,8 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
+use Joaopaulolndev\FilamentEditProfile\Pages\EditProfilePage;
 
 class AppPanelProvider extends PanelProvider
 {
@@ -63,7 +66,31 @@ class AppPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->plugins([
-                //
+                FilamentEditProfilePlugin::make()
+                    ->slug('my-profile')
+                    ->setTitle(__('My Profile'))
+                    ->setNavigationLabel(__('My Profile'))
+                    ->setNavigationGroup(__('Group Profile'))
+                    ->setIcon('heroicon-o-user')
+                    ->setSort(10)
+                    ->shouldRegisterNavigation(false)
+                    ->shouldShowEmailForm()
+                    ->shouldShowLocaleForm(options: [
+                        'pt_BR' => __('🇧🇷 Português'),
+                        'en' => __('🇺🇸 Inglês'),
+                        'es' => __('🇪🇸 Espanhol'),
+                    ])
+                    ->shouldShowThemeColorForm()
+                    ->shouldShowSanctumTokens()
+                    ->shouldShowMultiFactorAuthentication()
+                    ->shouldShowBrowserSessionsForm()
+                    ->shouldShowAvatarForm(),
+            ])
+            ->userMenuItems([
+                'profile' => Action::make('profile')
+                    ->label(fn (): string => __('My Profile'))
+                    ->url(fn (): string => EditProfilePage::getUrl())
+                    ->icon('heroicon-m-user-circle'),
             ])
             ->unsavedChangesAlerts()
             ->passwordReset()
